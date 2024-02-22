1139ba5315be13aea5f748a5c5f894d53c22a806
RandelYuan <=>
2024-02-22 21:14:08 +0800

    feat: 首页

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,29 +1,46 @@
-<template>
-	<view class="container">
-		
-		<view class="intro">本项目已包含uni ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。</view>
-		<text class="intro">详见：</text>
-		<uni-link :href="href" :text="href"></uni-link>
-	</view>
-</template>
-
 <script>
+	import Nav from '@/layout/nav.vue'
+	import Footer from '@/layout/footer/index.vue'
+
 	export default {
+		components: {
+			Nav,
+			Footer
+		},
 		data() {
 			return {
 				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
 			}
-		},
-		methods: {
-
 		}
 	}
 </script>
 
-<style>
-	.container {
-		padding: 20px;
-		font-size: 14px;
-		line-height: 24px;
+<template>
+	<div class="page">
+		<div class="nav-container">
+			<Nav />
+		</div>
+
+		<div class="body">
+			<div class="body-wrapper">
+				这是内容
+			</div>
+		</div>
+
+		<div class="footer-container">
+			<Footer />
+		</div>
+	</div>
+</template>
+
+<style scoped lang="scss">
+	.body {
+		height: 800px;
+		background-color: #0000FF0F;
+
+		.body-wrapper {
+			margin: 0 auto;
+			width: 1200px;
+		}
 	}
 </style>
